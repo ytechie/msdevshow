30c492d55233ae4b452968b7a02044d51e3c2b95
Jason Young <jason@ytechie.com>
2015-09-06 22:14:57 -0500

    Hide git output

--- scripts/deploy.sh
+++ scripts/deploy.sh
@@ -11,4 +11,6 @@ git commit -m "Deploy"
 
 # We redirect any output to
 # /dev/null to hide any sensitive credential data that might otherwise be exposed.
-git push --force "https://${git_user}:${git_password}@${git_target}" master:master 
+git push --force "https://${git_user}:${git_password}@${git_target}" master:master > /dev/null 2>&1
+
+exit 0
