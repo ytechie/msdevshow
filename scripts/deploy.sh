fe4c637c2b2fffb9ad1f0f54a93977a002b6e9fb
Jason Young <jason@ytechie.com>
2017-06-22 20:04:32 -0700

    Don't eat messages for troubleshooting

--- scripts/deploy.sh
+++ scripts/deploy.sh
@@ -12,7 +12,7 @@ git commit -m "Deploy"
 # We redirect any output to
 # /dev/null to hide any sensitive credential data that might otherwise be exposed.
 echo 'Pushing to Git'
-git push --force "https://${git_user}:${git_password}@${git_target}" master:master > /dev/null 2>&1
+git push --force "https://${git_user}:${git_password}@${git_target}" master:master #> /dev/null 2>&1
 echo 'Git push complete'
 
 exit 0
